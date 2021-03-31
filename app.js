e561c83719fcf48fb41e9a090614fc6e2e2be839
majid shahid <majidshahid474@gmail.com>
2021-03-31 15:55:21 -0700

    js

---
+++ app.js
@@ -0,0 +1,128 @@
+// var a ="majid";
+// var b =2;
+// document.write(a,b);
+
+
+
+// var a =2;
+// var b =3;
+// console.log(a*b)
+
+// var a =2;
+// var b =3;
+// alert(a+b)
+
+// var a ="majid";
+// var b ="shahid";
+// // document.write(a,b)
+// document.write(a+" "+b)
+
+
+//NAN is called NOT A NUMBER
+
+// var a=3;
+// var b="majid"
+// document.write(a*b)
+
+// STRING OR INTERGER
+//  var a ="I am srting " ;
+//  document.write(a +"<br>");
+
+
+//  var b = 3 
+
+//  document.write("number is interger like b " + b )
+
+// line breake 
+
+
+// document.write("majid shahid"+"<br>" +"umar zahid");
+
+
+// var num = 90;
+// var popularNumber = num + 200;
+// document.write(popularNumber)
+
+
+
+// PER INCREMENT
+// var  a =1;
+// var b =++a;
+// document.write(b)
+
+// POST INCREMENT
+
+// var a =10;
+// var b = 1;
+// var c = b + ++a 
+// document.write(c)
+
+
+
+// PER decrement
+
+// var a =10;
+// var b=--a;
+// document.write(b)
+
+// Post decrement
+ 
+// var a =10;
+// var b = 1;
+// var c = b + --a 
+// document.write(c)
+
+
+
+// var a =10;
+// var  b = ++a + ++a + a-- + a++ + --a;
+// document.write(b)
+
+
+//prompt
+        // for string 
+// var a =prompt("enter your value")
+// document.write(a)
+    //   for interger 
+// var a =+prompt("enter your number");
+// var b =+prompt("enter your second Number");
+// document.write(a+b)
+
+
+// if else statement
+
+// var a =prompt("enter your name");
+// if (a=="majid"){
+//     alert("ok")
+// }else{
+//     alert("sorry")
+// }
+
+
+
+// var a =prompt("enter your number");     (== chack value)
+// if (a==2){
+//     document.write("ok")
+// }else{
+//     document.write("sorry")
+// }
+
+//  var a =+prompt("enter your number");  //( === is chack type and value)
+// if (a===2){
+//     document.write("ok")
+// }else{
+//     document.write("sorry")
+// }
+
+
+
+// var a =prompt("enter your name ");
+// if (a== "majid"){
+//     document.write("welcome  majid")
+// }else if (a== "umar"){
+//     document.write("welcome umar")
+// }
+// else{
+//     document.write("sorry")
+// }
+
