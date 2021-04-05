68bd877709fc53b935235bd2b43f06534fb805d8
majid shahid <majidshahid474@gmail.com>
2021-04-05 15:47:54 -0700

    js

---
+++ class 2/app.js
@@ -0,0 +1,25 @@
+// var names=prompt("enter your value");
+// var chk ="";
+// for(var i=names.length-1;i>=0;i--){
+//     chk += names[i]
+// }
+// if (names==chk ){
+//     document.write("ok")
+// }else{
+//     document.write("wrong")
+// }
+
+// for (var i =1;i<100;i=i+10){
+//   for (var j=i; j< i+10;j++){
+//       document.write(j)
+//   }
+//   document.write("<br>")
+// }
+
+
+for (var i=1;i<100;i=i+10){
+    for (var j=i;j< i+10;j++){
+        document.write(j)
+    }
+    document.write("<br>")
+}
