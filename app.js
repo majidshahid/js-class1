3921000ae6323c5eb01562016633f531d032d841
majid shahid <majidshahid474@gmail.com>
2021-04-21 16:23:02 -0700

    js

--- app.js
+++ app.js
@@ -341,3 +341,55 @@
     //  var a =setDate('apr 2,1900');
 //        var b = a.getHours()
 // console.log(b)
+
+
+
+// var num1 = 3.5;
+
+// console.log (num1);
+
+// num1 = Math.round (num1);
+
+// console.log (num1);
+
+// var a = new Date();
+// var c =a.toString()
+// var b =c.slice(0,3)
+// console.log(b)
+// var month =a.getMonth()
+// console.log(month)
+
+
+// var fsubject=+prompt("enter your first subject number out of 100");
+// var ssubject=+prompt("enter your first subject number out of 100");
+// var tsubject=+prompt("enter your first subject number out of 100");
+// var f4subject=+prompt("enter your first subject number out of 100");
+// var f5subject=+prompt("enter your first subject number out of 100");
+
+
+// function per(){
+//     var a=fsubject+ssubject+tsubject+f4subject+f5subject;
+// var b =(a/500*100)
+// document.write(b+""+"%")
+
+// }
+
+// per()
+
+// var fsubject=+prompt("enter your first subject number out of 100");
+// var ssubject=+prompt("enter your first subject number out of 100");
+// var tsubject=+prompt("enter your first subject number out of 100");
+// var f4subject=+prompt("enter your first subject number out of 100");
+// var f5subject=+prompt("enter your first subject number out of 100");
+
+
+// function per(){
+//     var a=fsubject+ssubject+tsubject+f4subject+f5subject;
+// var b =(a/500*100)
+// document.write(b+" "+ "%" + "call in function "+"<br> ")
+// return b 
+
+// }
+
+// var a =per()
+// document.write(a+" "+ "%")
